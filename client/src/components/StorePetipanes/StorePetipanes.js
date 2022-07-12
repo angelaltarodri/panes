cb0d79d2cbb61188ef066e71b9a628998748409c
Angel <angelaltarodri@gmail.com>
2022-07-11 21:28:47 -0500

    a medio codigo para una mejor formula con amount

--- client/src/components/StorePetipanes/StorePetipanes.js
+++ client/src/components/StorePetipanes/StorePetipanes.js
@@ -5,16 +5,24 @@ import "./StorePetipanes.css"
 import PetipanSimulator from '../PetipanSimulator/PetipanSimulator'
 
 export default function StorePetipanes() {
-  const {cant} = useParams()
-  const [monto, setmonto] = useState(0)
-  const pedidos = useSelector(store => store.orderReducer.petipanes) 
-  const history = useHistory()  
-  const gohome = () => history.push("/tienda")
-  const items = useSelector(store => store.itemsReducer.petipanesSabores)  
-  const nOfItems = Object.keys(pedidos[0])
   const dispatch = useDispatch()
+  const history = useHistory()  
+  const {cant} = useParams()
+  const sabores = useSelector(store => store.itemsReducer.petipanesSabores)  
+  const paquetes = useSelector(store => store.itemsReducer.petipanesPaquetes)  
+  const paqueteElegido = paquetes.find(paquete => paquete.paqueteNumero == cant)
 
-  const itemsPlusFive = items.map(item=> {
+  useEffect(()=>{
+    dispatch({type:'AMOUNT_PETIPANES', payload: paqueteElegido.paquetePrecio})
+  },[])
+
+  const petipanesOrden = useSelector(store => store.orderReducer.petipanesOrden) 
+  const petipanesMonto = useSelector(store => store.orderReducer.petipanesMonto) 
+
+  const gohome = () => history.push("/tienda")
+  const nOfItems = Object.keys(petipanesOrden[0])
+
+  const itemsPlusFive = sabores.map(item=> {
     if(!item.itemType.includes("adicional"))
     return <div className="StorePetipanes_opciones" onClick={()=>addToCart(item.itemCode)} style={{backgroundColor:item.itemBackgroundColor, color:item.itemTextColor}}>
       <div>
@@ -27,24 +35,25 @@ export default function StorePetipanes() {
   })
 
   const addToCart = (value) => {
-    pedidos[0][value] ? pedidos[0][value] += 5 : pedidos[0][value] = 5
-    dispatch({type:'ORDER_PETIPANES', payload: pedidos})
-    console.log(pedidos)
+    petipanesOrden[0][value] ? petipanesOrden[0][value] += 5 : petipanesOrden[0][value] = 5
+    let saborsito = sabores.find(sabor => sabor.itemCode == value)
+    dispatch({type:'AMOUNT_PETIPANES', payload: petipanesMonto + saborsito.itemPriceChange})
+    dispatch({type:'ORDER_PETIPANES', payload: petipanesOrden})
   }
 
-  const itemsOnOrder = items.map(item => {
+  const itemsOnOrder = sabores.map(item => {
     const deleteItem = () => {
-      delete(pedidos[0][item.itemCode])
-      dispatch({type:'ORDER_PETIPANES', payload: pedidos})
+      delete(petipanesOrden[0][item.itemCode])
+      dispatch({type:'ORDER_PETIPANES', payload: petipanesOrden})
     }
     const cantidaD = (d) => {
       return <div className={`StorePetipanes_cantidad ` + d}>
-        <div className={`StorePetipanes_cantidad_izq`}> {pedidos[0][item.itemCode]}</div>
+        <div className={`StorePetipanes_cantidad_izq`}> {petipanesOrden[0][item.itemCode]}</div>
         <div className="StorePetipanes_cantidad_mid">{item.itemShortName}</div>
         <div className={`StorePetipanes_cantidad_der`} onClick={deleteItem}> x </div>
       </div>
     }
-    if(pedidos[0][item.itemCode]){
+    if(petipanesOrden[0][item.itemCode]){
       if(nOfItems.length == 1) return cantidaD("StorePetipanes_cantida") 
       else return cantidaD("")
     } 
@@ -59,7 +68,7 @@ export default function StorePetipanes() {
           {itemsPlusFive}
         </div>
         <div>Monto:</div>
-        <div>{monto}</div>
+        <div>{petipanesMonto}</div>
         <div className="StorePetipanes_cantidades">
           <div>En tu pedido hay: </div>
           <div>
@@ -74,7 +83,7 @@ export default function StorePetipanes() {
       {nOfItems == "" ? null : 
         <div>
           <div> Tu caja quedaría así: </div>
-          <PetipanSimulator pedido={pedidos[0]}/> 
+          <PetipanSimulator pedido={petipanesOrden[0]} /> 
         </div>
       }
     </div>
