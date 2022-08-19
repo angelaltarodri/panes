a26d5f9a5b32fe7fd4be5a603b2e30a0401f69d8
Angel <angelaltarodri@gmail.com>
2022-08-18 22:55:35 -0500

    loginview

--- client/src/components/StoreNav/StoreNav.js
+++ client/src/components/StoreNav/StoreNav.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import{useHistory} from "react-router-dom";
+import{useNavigate} from "react-router-dom";
 import {useSelector, useDispatch} from 'react-redux'
 import "./StoreNav.css"
 import logo from '../Images/logo_400x400.png'
 import {sumaMontos} from '../Cart/SumaMontos'
 export default function StoreNav() {
     const carrito = useSelector(store => store.cartReducer.carrito) 
-    const history = useHistory()    
-    const gohome = () => history.push("/")
-    const goUser = () => history.push("/tienda/login")
-    const goCart = () => history.push("/tienda/carrito")
+    const navigate = useNavigate()    
+    const gohome = () => navigate("/")
+    const goUser = () => navigate("/tienda/login")
+    const goCart = () => navigate("/tienda/carrito")
     const sumaMonto = sumaMontos()
     return (    
         <div className="StoreNav_container">
